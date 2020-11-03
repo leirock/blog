c963f80dc771d8ddb89aa82afdc47b426c422e8a
leirock <19180725+leirock@users.noreply.github.com>
2020-11-04 04:02:33 +0800

    Blog v3.1.1

--- hexo.sh
+++ hexo.sh
@@ -22,16 +22,10 @@ if [ "$answer" == "1" ] || [ "$answer" == "" ]; then
 	echo " "
 	printf "\033[32mINFO \033[0m 启动本地预览...\n"
 	echo " "
-	sed -i "" 's#css: https://website-1256060851.cos.ap-hongkong.myqcloud.com/assets/css-blog#css: css#g' _config.next.yml
-	sed -i "" 's#js: https://website-1256060851.cos.ap-hongkong.myqcloud.com/assets/js#js: js#g' _config.next.yml
-	sed -i "" 's#images: https://website-1256060851.cos.ap-hongkong.myqcloud.com/assets/images#images: images#g' _config.next.yml
 	sed -i "" '26s/imageLink/imageLink.replace(\/\![0-9]{3,}x\/,"")/' node_modules/hexo-theme-next/source/js/utils.js
 	hexo s
 	hexo clean
 	sed -i "" '26s/.replace(\/\!\[0-9\]{3,}x\/,\"\")//' node_modules/hexo-theme-next/source/js/utils.js
-	sed -i "" 's#css: css#css: https://website-1256060851.cos.ap-hongkong.myqcloud.com/assets/css-blog#g' _config.next.yml
-	sed -i "" 's#js: js#js: https://website-1256060851.cos.ap-hongkong.myqcloud.com/assets/js#g' _config.next.yml
-	sed -i "" 's#images: images#images: https://website-1256060851.cos.ap-hongkong.myqcloud.com/assets/images#g' _config.next.yml
 	echo " "
 	exec ${HexoPath}/hexo.sh
 else
