89930bfb91d426cb99f41ced8e665ec744f5b919
leirock <19180725+leirock@users.noreply.github.com>
2022-03-15 07:45:17 +0800

    postmeta && debug

--- scripts/friendslist.js
+++ scripts/friendslist.js
@@ -1,8 +1,18 @@
+// Generate friends list in JSON format
 // See https://github.com/SukkaW/Friends
 
+const yaml = require('js-yaml');
+const { readFileSync, mkdirSync, writeFileSync } = require('fs');
+
+// Get document, or throw exception on error
 try {
-    const doc = yaml.load(readFileSync('/source/friends/friendslist.yml', 'utf8'));
-    writeFileSync('/source/friends/friendslist.json', JSON.stringify(doc));
+    const doc = yaml.load(readFileSync('./source/friends/friendslist.yml', 'utf8'));
+    try {
+        mkdirSync('./public/friends/');
+    } catch ({ code }) {
+        if (code !== 'EEXIST') throw code;
+    }
+    writeFileSync('./public/friends/friendslist.json', JSON.stringify(doc));
 } catch (e) {
     console.error(e);
 }
