2daf2c2f889ae46a25fad33ef9290adcc83238b6
leirock <19180725+leirock@users.noreply.github.com>
2022-04-25 16:50:19 +0800

    linklist2json script

--- scripts/linklist2json.js
+++ scripts/linklist2json.js
@@ -1,24 +1,24 @@
 // Generate friends list in JSON format
 const yaml = require('js-yaml');
+const path = require('path');
 const { readdirSync, readFileSync, mkdirSync, writeFileSync } = require('fs');
 
 const src = './source/links/';
-const dist = './public/links/';
+const files = readdirSync(src);
 
+const dist = './public/links/';
 try {
     mkdirSync(dist, {recursive: true});
 } catch ({ code }) {
     if (code !== 'EEXIST') throw code;
 }
 
-var files = readdirSync(src);
-var path = require('path');
-
 for(var i in files) {
     if(path.extname(files[i]) === ".yml") {
         try {
-            const doc = yaml.load(readFileSync(src + files[i], 'utf8'));
-            writeFileSync(dist + files[i].slice(0, -3) + 'json', JSON.stringify(doc));
+            var doc = yaml.load(readFileSync(src + files[i], 'utf8'));
+            var output = dist + files[i].slice(0, -4) + '.json';
+            writeFileSync(output, JSON.stringify(doc));
         } catch (e) {
             console.error(e);
         }
