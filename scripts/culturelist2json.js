b1d16756d15ca3aed6e68bcef7a47f696b94f36d
leirock <19180725+leirock@users.noreply.github.com>
2022-05-06 23:54:59 +0800

    format scripts

--- scripts/culturelist2json.js
+++ scripts/culturelist2json.js
@@ -1,24 +1,33 @@
 const yaml = require('js-yaml');
 const path = require('path');
-const { readdirSync, readFileSync, mkdirSync, writeFileSync } = require('fs');
+const {
+    readdirSync,
+    readFileSync,
+    mkdirSync,
+    writeFileSync
+} = require('fs');
 
 const cultureSrc = './source/' + hexo.config.custom_page_path.culture + '/';
 const cultureDist = './public/' + hexo.config.custom_page_path.culture + '/';
 //read subfolders in culture and define as mediaType
 const mediaType = readdirSync(cultureSrc);
 
-for(var i in mediaType) {
+for (var i in mediaType) {
     if (path.extname(mediaType[i]) === "") { //foldrs only, exclude files 
         const mediaTypeSrc = cultureSrc + mediaType[i] + '/';
         const mediaTypeDist = cultureDist + mediaType[i] + '/';
         try {
-            mkdirSync(mediaTypeDist, {recursive: true});
-        } catch ({ code }) {
+            mkdirSync(mediaTypeDist, {
+                recursive: true
+            });
+        } catch ({
+            code
+        }) {
             if (code !== 'EEXIST') throw code;
         }
-        
+
         const files = readdirSync(mediaTypeSrc);
-        for(var j in files) {
+        for (var j in files) {
             if (path.extname(files[j]) === ".yml") {
                 try {
                     var doc = yaml.load(readFileSync(mediaTypeSrc + files[j], 'utf8'));
--- scripts/layout-tags.js
+++ scripts/layout-tags.js
@@ -8,7 +8,8 @@
 //     hexo.theme.setView('page.njk', file);
 // })
 
-// 初始化对象存储域名cosDomain
+
+// 初始化对象存储域名 cosDomain
 console.log('\n-------------------------------------------------\n \
 COS Domain is: ' + hexo.config.cos_domain + '（腾讯云）\
 \n-------------------------------------------------\n')
@@ -16,48 +17,61 @@ COS Domain is: ' + hexo.config.cos_domain + '（腾讯云）\
 const cosDomain = hexo.config.cos_domain;
 // preconnect
 hexo.extend.injector.register('head_begin', () => {
-  return '<link rel="preconnect" href="'+ cosDomain +'" crossorigin="">';
+    return '<link rel="preconnect" href="' + cosDomain + '" crossorigin="">';
 });
+
 
 // 着重号
-hexo.extend.tag.register('dot', function(args) {
-  return `<em class="emphasis-point">${args.join(' ')}</em>`;
+hexo.extend.tag.register('dot', function (args) {
+    return `<em class="emphasis-point">${args.join(' ')}</em>`;
 });
+
 
 // 友链列表
-hexo.extend.tag.register('linklist', function(args) {
-  return `<div class="link-list" src="${args}.json"></div>`;
+hexo.extend.tag.register('linklist', function (args) {
+    return `<div class="link-list" src="${args}.json"></div>`;
 });
 
+
 // 文艺清单
-hexo.extend.tag.register('culturelist', function(args) {
-  const coverSrc = cosDomain + '/' + hexo.config.custom_page_path.culture + '/' + args + '/';
-  const jsonSrc = args + '-list.json';
-  return `<div class="culture-list" cover-src="${coverSrc}" json-src="${jsonSrc}"></div>`;
+hexo.extend.tag.register('culturelist', function (args) {
+    const coverSrc = cosDomain + '/' + hexo.config.custom_page_path.culture + '/' + args + '/';
+    const jsonSrc = args + '-list.json';
+    return `<div class="culture-list" cover-src="${coverSrc}" json-src="${jsonSrc}"></div>`;
 });
+
 
 // 相册
 // 如果输入是两个值，那么用 args[0]，args[1] 分别代表
-hexo.extend.tag.register('album', function(args) {
-  const photoSrc = cosDomain + '/' + hexo.config.custom_page_path.album + '/' + args + '/';
-  const jsonSrc = photoSrc + args + '-list.json';
-  return `<style>.post-block{padding-left:10px;padding-right:10px;}</style>
-  <div class="album" photo-src="${photoSrc}" json-src="${jsonSrc}"></div>`;
+hexo.extend.tag.register('album', function (args) {
+    const photoSrc = cosDomain + '/' + hexo.config.custom_page_path.album + '/' + args + '/';
+    const jsonSrc = photoSrc + args + '-list.json';
+    return `
+        <style>
+            .post-block { 
+                padding-left: 10px;
+                padding-right: 10px;
+            }
+        </style>
+        <div class="album" photo-src="${photoSrc}" json-src="${jsonSrc}"></div>
+    `;
 });
 
+
 // 子页面列表
-hexo.extend.tag.register('subpagebox', function([args, delimiter = '|', comment = '%'], content) {
-  const links = content.split('\n').filter(line => line.trim() !== '').map(line => {
-    const item = line.split(delimiter).map(arg => arg.trim());
-    const imageSource = cosDomain + '/' + args + '/' + item[1] + '/' + item[2];
-    if (item[0][0] === comment) return '';
-    return `
-      <div class="subpage-box-cover">
-        <a href="${item[1]}/">
-          <img alt="${item[0]}" src="${imageSource}">
-          <p class="image-caption">${item[0]}</p>
-        </a>
-      </div>`;
-  });
-  return `<div class="subpage-box">${links.join('')}</div>`;
+hexo.extend.tag.register('subpagebox', function ([args, delimiter = '|', comment = '%'], content) {
+    const links = content.split('\n').filter(line => line.trim() !== '').map(line => {
+        const item = line.split(delimiter).map(arg => arg.trim());
+        const imageSource = cosDomain + '/' + args + '/' + item[1] + '/' + item[2];
+        if (item[0][0] === comment) return '';
+        return `
+            <div class="subpage-box-cover">
+                <a href="${item[1]}/">
+                    <img alt="${item[0]}" src="${imageSource}">
+                    <p class="image-caption">${item[0]}</p>
+                </a>
+            </div>
+        `;
+    });
+    return `<div class="subpage-box">${links.join('')}</div>`;
 }, true);
