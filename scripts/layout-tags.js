0c9378f1c728190a5fb749efbc8d7bdb58cf56b6
leirock <19180725+leirock@users.noreply.github.com>
2022-05-06 23:42:36 +0800

    update scripts

--- scripts/layout-tags.js
+++ scripts/layout-tags.js
@@ -8,43 +8,44 @@
 //     hexo.theme.setView('page.njk', file);
 // })
 
+// 初始化对象存储域名cosDomain
 console.log('\n-------------------------------------------------\n \
 COS Domain is: ' + hexo.config.cos_domain + '（腾讯云）\
 \n-------------------------------------------------\n')
-const cosDomain = hexo.config.cos_domain;
 
+const cosDomain = hexo.config.cos_domain;
+// preconnect
 hexo.extend.injector.register('head_begin', () => {
   return '<link rel="preconnect" href="'+ cosDomain +'" crossorigin="">';
 });
 
-//着重号
+// 着重号
 hexo.extend.tag.register('dot', function(args) {
   return `<em class="emphasis-point">${args.join(' ')}</em>`;
 });
 
-//友链列表
+// 友链列表
 hexo.extend.tag.register('linklist', function(args) {
   return `<div class="link-list" src="${args}.json"></div>`;
 });
 
-//文艺清单
+// 文艺清单
 hexo.extend.tag.register('culturelist', function(args) {
-  //const cosDomain = 'https://cos.pinlyu.com';
-  const coverSrc = cosDomain + '/culture/' + args + '/';
+  const coverSrc = cosDomain + '/' + hexo.config.custom_page_path.culture + '/' + args + '/';
   const jsonSrc = args + '-list.json';
   return `<div class="culture-list" cover-src="${coverSrc}" json-src="${jsonSrc}"></div>`;
 });
 
-//相册
-//如果输入是两个值，那么用 args[0]，args[1] 分别代表
+// 相册
+// 如果输入是两个值，那么用 args[0]，args[1] 分别代表
 hexo.extend.tag.register('album', function(args) {
-  const photoSrc = cosDomain + '/album/' + args + '/';
-  const jsonSrc = photoSrc + 'photolist.json';
+  const photoSrc = cosDomain + '/' + hexo.config.custom_page_path.album + '/' + args + '/';
+  const jsonSrc = photoSrc + args + '-list.json';
   return `<style>.post-block{padding-left:10px;padding-right:10px;}</style>
   <div class="album" photo-src="${photoSrc}" json-src="${jsonSrc}"></div>`;
 });
 
-//子页面列表
+// 子页面列表
 hexo.extend.tag.register('subpagebox', function([args, delimiter = '|', comment = '%'], content) {
   const links = content.split('\n').filter(line => line.trim() !== '').map(line => {
     const item = line.split(delimiter).map(arg => arg.trim());
