ca1e8c88545e6daf37ba9635447969d294c9105e
leirock <19180725+leirock@users.noreply.github.com>
2022-05-11 21:19:25 +0800

    upgrade hexo, update scripts

--- scripts/culturelist2json.js
+++ scripts/culturelist2json.js
@@ -1,34 +1,33 @@
 const yaml = require('js-yaml');
 const path = require('path');
-const { readdirSync, readFileSync, mkdirSync, writeFileSync } = require('fs');
+const { readdirSync, readFileSync, mkdirSync, writeFileSync, stat } = require('fs');
 
 const cultureSrc = './source/' + hexo.config.custom_page_path.culture + '/';
 const cultureDist = './public/' + hexo.config.custom_page_path.culture + '/';
-//read subfolders in culture and define as mediaType
 const mediaType = readdirSync(cultureSrc);
 
 for (var i in mediaType) {
-    //foldrs only, exclude files 
-    if ( path.extname(mediaType[i]) === "" && !mediaType[i].includes('DS_Store') ) {
-        const mediaTypeSrc = cultureSrc + mediaType[i] + '/';
-        const mediaTypeDist = cultureDist + mediaType[i] + '/';
-        try {
-            mkdirSync(mediaTypeDist, { recursive: true });
-        } catch ({ code }) {
-            if (code !== 'EEXIST') throw code;
+    stat(cultureSrc + mediaType[i], function (err, stats) {
+        if (err) {
+            return;
         }
+        if (stats.isDirectory()) {
+            const mediaTypeDist = cultureDist + mediaType[i] + '/';
+            try {
+                mkdirSync(mediaTypeDist, { recursive: true });
+            } catch ({ code }) {
+                if (code !== 'EEXIST') throw code;
+            }
 
-        const files = readdirSync(mediaTypeSrc);
-        for (var j in files) {
-            if (path.extname(files[j]) === ".yml") {
-                try {
+            const mediaTypeSrc = cultureSrc + mediaType[i] + '/';
+            const files = readdirSync(mediaTypeSrc);
+            for (var j in files) {
+                if (path.extname(files[j]) === ".yml") {
                     var doc = yaml.load(readFileSync(mediaTypeSrc + files[j], 'utf8'));
                     var output = mediaTypeDist + files[j].slice(0, -4) + '.json';
                     writeFileSync(output, JSON.stringify(doc));
-                } catch (e) {
-                    console.error(e);
                 }
             }
         }
-    }
+    })
 }
